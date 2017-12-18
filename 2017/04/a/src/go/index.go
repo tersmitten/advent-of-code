34ef039936d92ba5ce7c6d5db1b4d4c9a8e8edab
Mischa ter Smitten <mischa@tersmitten.nl>
2017-12-18 22:45:58 +0100

    2017, day 4, Go

---
+++ 2017/04/a/src/go/index.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "strings"
+  "regexp"
+  "errors"
+)
+
+func SliceCountValues(values []string) (valueCount map[string]int) {
+  valueCount = make(map[string]int)
+  for _, value := range values {
+    valueCount[value] += 1
+  }
+
+  return valueCount
+}
+
+func MapMax(values map[string]int) (max int, e error) {
+  numValues := len(values)
+  if numValues == 0 {
+    return 0, errors.New("Cannot detect a maximum value in an empty slice")
+  }
+
+  mapValues := []int{}
+  for _, value := range values {
+    mapValues = append(mapValues, value)
+  }
+
+  max = mapValues[0]
+  for _, value := range mapValues {
+    if value > max {
+      max = value
+    }
+  }
+
+  return max, nil
+}
+
+func main() {
+  scanner := bufio.NewScanner(os.Stdin)
+  passphraseList := make([][]string, 0)
+  for scanner.Scan() {
+    row := regexp.MustCompile("\\s+").Split(strings.Trim(scanner.Text(), ""), -1)
+    passphraseList = append(passphraseList, row)
+  }
+
+  if err := scanner.Err(); err != nil {
+    fmt.Fprintln(os.Stderr, "Error reading from stdin:", err)
+  }
+
+  sum := 0
+  for _, row := range passphraseList {
+    wordCounts := SliceCountValues(row)
+    if max, _ := MapMax(wordCounts); max == 1 {
+      sum += 1;
+    }
+  }
+
+  fmt.Println(sum)
+}
--- 2017/04/b/README.md
+++ 2017/04/b/README.md
@@ -1,4 +1,8 @@
-# php
+# implementations
+
+## php
+
+```sh
 while true; do
   diff -qw <(src/php/index.php < tests/input1.txt) <(cat tests/output1.txt);
   diff -qw <(src/php/index.php < tests/input2.txt) <(cat tests/output2.txt);
@@ -7,8 +11,31 @@ while true; do
   diff -qw <(src/php/index.php < tests/input5.txt) <(cat tests/output5.txt);
   sleep 2;
 done
+```
 
+```sh
 while true; do
   src/php/index.php < tests/input0.txt | tee tests/output0.txt;
   sleep 2;
 done
+```
+
+## go
+
+```sh
+while true; do
+  diff -qw <(go run src/go/index.go < tests/input1.txt) <(cat tests/output1.txt);
+  diff -qw <(go run src/go/index.go < tests/input2.txt) <(cat tests/output2.txt);
+  diff -qw <(go run src/go/index.go < tests/input3.txt) <(cat tests/output3.txt);
+  diff -qw <(go run src/go/index.go < tests/input4.txt) <(cat tests/output4.txt);
+  diff -qw <(go run src/go/index.go < tests/input5.txt) <(cat tests/output5.txt);
+  sleep 2;
+done
+```
+
+```sh
+while true; do
+  go run src/go/index.go < tests/input0.txt | tee tests/output0.txt;
+  sleep 2;
+done
+```
