c6d6ed078c6a333e6f37649d24b715481edc6b46
Mischa ter Smitten <mischa@tersmitten.nl>
2018-12-11 22:51:21 +0100

    2018, day 11(a), PHP

---
+++ 2018/11-chronal-charge/a/src/php/index.php
@@ -0,0 +1,91 @@
+#!/usr/bin/env php
+<?php
+$gridSize = 300;
+
+$f = fopen('php://stdin', 'r');
+$gridSerialNumber = trim(fgets($f));
+fclose($f);
+
+assert(getPowerLevel(3, 5, 8) === 4);
+assert(getPowerLevel(122, 79, 57) === -5);
+assert(getPowerLevel(217, 196, 39) === 0);
+assert(getPowerLevel(101,153, 71) === 4);
+
+assert(getAdjacent(2, 2) === [
+	// topLeft
+	[1, 1],
+	// top
+	[2, 1],
+	// topRight
+	[3, 1],
+	// midLeft
+	[1, 2],
+	// midRight
+	[3, 2],
+	// botLeft
+	[1, 3],
+	// bot
+	[2, 3],
+	// botRight
+	[3, 3],
+]);
+
+$maximumPowerLevel = PHP_INT_MIN;
+$topLeftFuelCell = [0, 0];
+
+$range = range(2, $gridSize - 1);
+foreach ($range as $y) {
+	foreach ($range as $x) {
+		$powerLevel = $totalPowerLevel = getPowerLevel($x, $y, $gridSerialNumber);
+		$adjacentFuelCells = getAdjacent($x, $y);
+		foreach ($adjacentFuelCells as $adjacentFuelCell) {
+			list($aX, $aY) = $adjacentFuelCell;
+			$totalPowerLevel += getPowerLevel($aX, $aY, $gridSerialNumber);
+		}
+
+		if ($totalPowerLevel > $maximumPowerLevel) {
+			$maximumPowerLevel = $totalPowerLevel;
+			list($aX, $aY) = $adjacentFuelCells[0];
+			$topLeftFuelCell = [$aX, $aY];
+		}
+	}
+}
+
+echo implode(',', $topLeftFuelCell) . PHP_EOL;
+
+function getPowerLevel(int $x, int $y, int $gridSerialNumber) : int {
+	$rackID = $x + 10;
+	$powerLevel = $rackID * $y;
+	$powerLevel += $gridSerialNumber;
+	$powerLevel *= $rackID;
+	$powerLevel = ($powerLevel / 100) % 10;
+	$powerLevel -= 5;
+
+	return $powerLevel;
+}
+
+function getAdjacent(int $x, int $y) : array {
+	$xPlusOne = $x + 1;
+	$xMinOne = $x - 1;
+	$yPlusOne = $y + 1;
+	$yMinOne = $y - 1;
+
+	return [
+		// topLeft
+		[$xMinOne, $yMinOne],
+		// top
+		[$x, $yMinOne],
+		// topRight
+		[$xPlusOne, $yMinOne],
+		// midLeft
+		[$xMinOne, $y],
+		// midRight
+		[$xPlusOne, $y],
+		// botLeft
+		[$xMinOne, $yPlusOne],
+		// bot
+		[$x, $yPlusOne],
+		// botRight
+		[$xPlusOne, $yPlusOne],
+	];
+}
