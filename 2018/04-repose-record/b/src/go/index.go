b455f619a665fe107eb316e9787d83660290e1c2
Mischa ter Smitten <mischa@tersmitten.nl>
2018-12-14 21:48:38 +0100

    2018, day 4(b), Go

---
+++ 2018/04-repose-record/b/src/go/index.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const Minute = 60
+
+func Strtotime(str string) (int64, error) {
+	layout := "2006-01-02 15:04"
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		return 0, err
+	}
+
+	return t.Unix(), nil
+}
+
+func main() {
+	lines := []string{}
+	asleep := make(map[int]int, 0)
+	asleepMinutes := make(map[int]map[int]int, 0)
+
+	var guard, maxFrequency, minuteMostSpendSleeping, guardThatIsMostFrequentlyAsleep int
+	var fallsAsleepTimestamp int64
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		lines = append(lines, strings.Trim(scanner.Text(), ""))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading from stdin:", err)
+	}
+
+	sort.Strings(lines)
+
+	for _, line := range lines {
+		shiftStartMatches := regexp.MustCompile("^\\[(.*)\\] Guard #(\\d+) begins shift$").FindStringSubmatch(line)
+		if len(shiftStartMatches) > 0 {
+			guard, _ = strconv.Atoi(shiftStartMatches[2])
+
+			continue;
+		}
+
+		fallsAsleepMatches := regexp.MustCompile("^\\[(.*)\\] falls asleep$").FindStringSubmatch(line)
+		if len(fallsAsleepMatches) > 0 {
+			fallsAsleep := fallsAsleepMatches[1]
+			fallsAsleepTimestamp, _ = Strtotime(fallsAsleep)
+
+			continue;
+		}
+
+		wakesUpMatches := regexp.MustCompile("^\\[(.*)\\] wakes up$").FindStringSubmatch(line)
+		if len(wakesUpMatches) > 0 {
+			wakesUp := wakesUpMatches[1]
+			wakesUpTimestamp, _ := Strtotime(wakesUp)
+
+			asleep[guard] += int(math.Abs(float64(fallsAsleepTimestamp - wakesUpTimestamp)) / Minute)
+
+			for timestamp := fallsAsleepTimestamp; timestamp < wakesUpTimestamp; timestamp += Minute {
+				minute := time.Unix(timestamp, 0).UTC().Minute()
+				if _, ok := asleepMinutes[guard]; !ok {
+					asleepMinutes[guard] = make(map[int]int, 0)
+				}
+
+				asleepMinutes[guard][minute] += 1
+			}
+
+			continue
+		}
+	}
+
+	for guard, asleepMinute:= range asleepMinutes {
+		for minute, frequency := range asleepMinute {
+			if frequency > maxFrequency {
+				maxFrequency = frequency
+				guardThatIsMostFrequentlyAsleep = guard
+				minuteMostSpendSleeping = minute
+			}
+		}
+	}
+
+	fmt.Println(guardThatIsMostFrequentlyAsleep * minuteMostSpendSleeping)
+}
